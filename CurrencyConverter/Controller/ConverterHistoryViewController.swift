40089cd948ab99c07c67001cb6fee929b80b3288
Bruno Kucevic <brunokuceivc@gmail.com>
2020-04-21 20:28:36 +0200

    added new VC and custom cell

---
+++ CurrencyConverter/Controller/ConverterHistoryViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ConverterHistoryViewController.swift
+//  CurrencyConverter
+//
+//  Created by Bruno Kučević on 21/04/2020.
+//  Copyright © 2020 Bruno Kučević. All rights reserved.
+//
+
+import UIKit
+
+class ConverterHistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CurrencyConverter/View/ConverterHistoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ConverterHistoryTableViewCell.swift
+//  CurrencyConverter
+//
+//  Created by Bruno Kučević on 21/04/2020.
+//  Copyright © 2020 Bruno Kučević. All rights reserved.
+//
+
+import UIKit
+
+class ConverterHistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
